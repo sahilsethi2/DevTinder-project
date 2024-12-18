b683e4c35c1efc19419d1bb245997e2ec10b6f2b
Sahil <sahil19908@gmail.com>
2024-12-18 16:34:00 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -5,16 +5,41 @@ const app = express(); //creating an instance of an expressjs application
 
 //request handler functions(routes, in short) && order of routes matters a lot!!
 
+//we can add as many route handlers as we want!!
+
+app.use(
+    "/user",
+    //next is done to run the 2nd Response!!
+    (req,res,next)=>{
+        console.log("handling the route user!!");
+        res.send("Response!!"); //suppose we are not handling it here and want to route to the next 2nd response then,
+        next(); //this forwards the router to the next request handler
+    },
+    //unless we dont put next function in this event handler, this event handler will run in itself
+    (req,res,next)=>{
+        console.log("Handling the route user 2!!");
+        res.send("2nd Response!!"); //sends the response back to Postman API and finishes the function thereafter
+        next();
+    },
+    (req,res,next)=>{
+        console.log("Handling the route user 3!!");
+        res.send("3rd Response!!"); //sends the response back to Postman API and finishes the function thereafter
+        next();
+    },
+    (req,res)=>{
+        console.log("Handling the route user 4!!");
+        res.send("4th Response!!"); //sends the response back to Postman API and finishes the function thereafter
+    }
+);
+
 //test this GET api call on POSTMAN API TESTER
 app.get("/user", (req,res)=>{
     console.log(req.params)
     res.send({firstName : "Akshay", lastName : "Saini"})
 })
-app.post("/user/:userId", (req,res)=>{
+app.post("/user", (req,res)=>{
     res.send("Saved data to the database successfully")
 })
-
-
 
 
 app.use("/hello/2", (req,res)=>{
