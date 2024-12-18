b605c22eb741440d300b1b00e09de0bcf49e87f6
Sahil <sahil19908@gmail.com>
2024-12-18 12:34:08 +0530

    added

--- homework.md
+++ homework.md
@@ -9,3 +9,13 @@ install nodemon and update scripts insde package.json
 what are dependencies
 what is the use of "-g" while npm install
 difference between caret and tilde (^ & ~)
+
+
+initialize git
+.gitignore
+create a remote repo on github
+push all the code to remote origin
+play with routes and route extensions ex. /hello, / , hello/2, /xyz
+order for the routes matters a lot
+install Postman app and make a workspace/collection > test API call
+write logic to handle GET, POST, PATCH, DELETE API calls and test them on POSTMan API!!
--- src/app.js
+++ src/app.js
@@ -3,10 +3,21 @@ const express = require("express");
 //basically creating a web server using express
 const app = express(); //creating an instance of an expressjs application
 
-//request handler function\
+//request handler functions(routes, in short) && order of routes matters a lot!!
 
-app.use("/", (req,res)=>{
-    res.send("Namaste from the dashboard!");
+//test this GET api call on POSTMAN API TESTER
+app.get("/user", (req,res)=>{
+    res.send({firstName : "Akshay", lastName : "Saini"})
+})
+app.post("/user", (req,res)=>{
+    res.send("Saved data to the database successfully")
+})
+
+
+
+
+app.use("/hello/2", (req,res)=>{
+    res.send("Aabra ka daabra!");
 });
 
 // write localhost:7777/hello to access this on browser:
@@ -14,10 +25,16 @@ app.use("/hello", (req,res)=>{
     res.send("Hello hello hello!");
 });
 
-
+//this will match all the HTTP method API calls to /test
 app.use("/test", (req,res)=>{
     res.send("Hello from the server!");
 });
+
+//this route overrides every other route 
+app.use("/", (req,res)=>{
+    res.send("Namaste from the dashboard!");
+});
+
 
 app.listen(7777, ()=>{
     console.log("Server id successfully listening on PORT:7777..")
